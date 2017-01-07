9081a51219bb583fe129d9481a9cd910c509c684
Layne Miao <layne00@gmail.com>
2017-01-07 12:22:00 -0800

    added cdn for bootstrap, changed background color

---
+++ client/src/Map.jsx
@@ -0,0 +1,73 @@
+import Helmet from "react-helmet";
+import _ from "lodash";
+import React from 'react';
+import moment from 'moment';
+import {GoogleMapLoader, GoogleMap, Marker, withGoogleMap} from "react-google-maps";
+
+
+
+
+const ShowMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+  >
+    {props.markers.map((marker,index) => (
+      <Marker key={index}
+        {...marker}
+      />
+    ))}
+  </GoogleMap>
+));
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleMapLoad(map) {
+    this._mapComponent = map;
+    if (map) {
+      console.log(map.getZoom());
+    }
+  }
+
+  getMarkers() {
+    var coordinates = this.props.listings.map(function (listing) {
+      var marker = {
+        position: {
+          lat: Number(listing.lat),
+          lng: Number(listing.long)
+        },
+        defaultAnimation: 2
+      }
+      return marker;
+    })
+    return coordinates;
+  }
+
+  render() {
+    var markers = this.getMarkers();
+    console.log(markers)
+    return (
+      <div style={{height: `100%`}}>
+        <Helmet
+          title="Getting Started"
+        />
+        <ShowMap
+          containerElement={
+            <div style={{width: 500, height: 500}} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+          onMapLoad={this.handleMapLoad.bind(this)}
+          markers={markers}
+        />
+      </div>
+    );
+  }
+}
+
+export default Map;

3f7be37990b373c991fe55f3b9f8d0bf344e60f7
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-01-06 16:02:03 -0800

    graph ql

--- gatewayServer/src/schema.js
+++ gatewayServer/src/schema.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios');
 
 const {
@@ -12,18 +13,58 @@ const {
   GraphQLFloat
 } = require('graphql');
 
+/*
+"title": "2 x 4GB RAM from MacBook Pro Mid 2012 MacBookPro9,2",
+"price": "$40",
+"cityName": " (noe valley)",
+"description": "<section id=\"postingbody\">\n        <div class=\"print-information print-qrcode-container\">\n            <p class=\"print-qrcode-label\">QR Code Link to This Post</p>\n            <div class=\"print-qrcode\" data-location=\"http://sfbay.craigslist.org/sfc/sop/5935323947.html\"></div>\n        </div>\n2 x 4GB RAM from MacBook Pro Mid 2012 MacBookPro9,2<br>\n<br>\n2 x 4.0GB 1600MHz DDR3L SO-DIMM PC12800 204 Pin<br>\n<br>\nFor Apple MacBook Pro models 2012-current:<br>\nMacBook Pro 13\" 2.5GHz dual-core Intel Core i5<br>\nMacBook Pro 13\" 2.9GHz dual-core Intel Core i7<br>\nMacBook Pro 15\" 2.3GHz quad-core Intel Core i7<br>\nMacBook Pro 15\" 2.6GHz quad-core Intel Core i7<br>\nMacBook Pro 15\" 2.7GHz quad-core Intel Core i7<br>\n<br>\nWorks with Model Identifiers: MacBookPro9,1 &amp; MacBookPro9,2<br>\n<br>\n<br>\n    </section>",
+"imageUrls": [
+  "https://images.craigslist.org/01212_793uG0K4ZZJ_600x450.jpg",
+  "https://images.craigslist.org/00l0l_fWCAT7DEkGD_600x450.jpg"
+],
+"lat": "37.758700",
+"long": "-122.433000",
+"postId": "5935323947",
+"postingUrl": "http://sfbay.craigslist.org/sfc/sop/5935323947.html",
+"postDate": "2016-12-27T17:37:48-0800",
+"updateDate": "2017-01-01T21:15:07-0800",
+"attributes": [
+  "<p class=\"attrgroup\">\n           <span>condition: <b>like new</b></span><br>\n   </p>"
+],
+"sellerUrl": "http://sfbay.craigslist.org/reply/sfo/sop/5935323947",
+"sellerName": "",
+"sellerPhoneNumber": "",
+"sellerEmail": "",
+"specs": []
+*/
+
 const ListingType = new GraphQLObjectType({
   name: 'Listing',
   description: 'A representation of elastic search listing',
   fields: () => ({
     description: {
       type: GraphQLString
-    }
+    },
+    title: { type: GraphQLString },
+    price: { type: GraphQLString },
+    cityName: { type: GraphQLString },
+    imageUrls: { type: new GraphQLList(GraphQLString) }, //since it is an array you need to make it a constructo functioin for list and pass it graphqlstrings
+    lat: { type: GraphQLString }, //*** may need to be string
+    long: { type: GraphQLString },
+    postId: { type: GraphQLString },
+    postingUrl: { type: GraphQLString },
+    postDate: { type: GraphQLString },
+    attributes: { type: new GraphQLList(GraphQLString) },
+    sellerUrl: { type: GraphQLString },
+    sellerName: { type: GraphQLString },
+    sellerPhoneNumber: { type: GraphQLString },
+    sellerEmail: { type: GraphQLString },
+    specs: { type: new GraphQLList(GraphQLString) }
   })
 });
 
 const HitsMetaData = new GraphQLObjectType({
-  name: 'Hits Meta Data',
+  name: 'HitsMetaData',
   description: 'Meta data associated with an elastic search',
   fields: () => ({
     total: { type: GraphQLInt },
@@ -33,11 +74,11 @@ const HitsMetaData = new GraphQLObjectType({
 })
 
 const ListingsHitResult = new GraphQLObjectType({
-  name: 'Listings hit result',
+  name: 'ListingsHitResult',
   description: 'Representaiton of a complete elastic search listing result',
   fields: () => ({
     metaData: {type: HitsMetaData},
-    results: new GraphQLList(ListingType)
+    results: {type: new GraphQLList(ListingType)}
   })
 })
 
@@ -63,7 +104,7 @@ const Query = new GraphQLObjectType({
         }
       },
       resolve: async (_, args) => {
-        let result;
+        let request;
         try {
           let postReq = {
             "query": {
@@ -75,12 +116,21 @@ const Query = new GraphQLObjectType({
           if (args.size) { postReq.size = args.size; }
           if (args.from) { postReq.from = args.from; }
 
-          result = await axios.post('http://louispvb.com:9200/cl/listing/_search?pretty', postReq);
+          request = await axios.post('http://louispvb.com:9200/cl/listing/_search?pretty', postReq);
         } catch(error) {
           console.error(error);
         }
-        result = result.data.hits.hits.map(listing => ({description: listing._source.description}));
-        return result;
+        results = request.data.hits.hits.map(listing => listing._source);
+        console.log(request.data)
+        let listingsHitResult = {
+          metaData: {
+            total: request.data.hits.total,
+            maxScore: request.data.hits.max_score,
+            timedOut: request.data.timed_out
+          }, 
+          results
+        }
+        return listingsHitResult;
       }
     }
   })
@@ -91,3 +141,4 @@ const schema = new GraphQLSchema({
 });
 
 module.exports = schema;
+
